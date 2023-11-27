c081f81869777c75b0dd8a52e5ba8451e623fafd
Anas_Ismail_HZ <100934197+AnasIsmail8996@users.noreply.github.com>
2023-11-27 17:01:24 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,43 @@
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+
+import Home from './Pages/Home/Home';
+import WhoIam from './Pages/WhoIam/WhoIam';
+import FirstPage from './Pages/FirstPage/FirstPage';
+import Education from './Pages/Education/Education';
+import Skills from './Pages/Skills/Skills';
+import Experiance from './Pages/Experiance/Experiance';
+import Project from './Pages/Project/Project';
+import Achievements from './Pages/Achievements/Achievements';
+import Services from './Pages/Services/Services';
+import Testimonials from './Pages/Testimonials/Testimonials';
+import Contacts from './Pages/Contacts/Contacts';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+         
+            <Route path="/" element={<Home />} />
+            <Route path="/firstpage" element={<FirstPage />} />
+            <Route path="/whoiam" element={<WhoIam />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experiance" element={<Experiance />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/contacts" element={<Contacts />} />
+           
+            <Route path="*" element={<Navigate to="/" />} />
+        
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
